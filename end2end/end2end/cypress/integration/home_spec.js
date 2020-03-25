a1e3ae4e72a36a19f1ccde2b5ed5adf0b2336945
Coconuts Development <development@coconuts.la>
2020-03-25 14:15:26 -0300

    Ejemplo Cypress

---
+++ end2end/end2end/cypress/integration/home_spec.js
@@ -0,0 +1,17 @@
+﻿describe('Test home page', function () {
+    it('chequear contenido "welcome"', function () {
+        cy.visit('https://localhost:5001')
+
+        cy.contains('Welcome')
+    })
+
+    it('click en "Privacy" navega a una nueva URL', () => {
+        cy.visit('https://localhost:5001')
+
+        cy.contains('Privacy').click()
+
+        // Deberia ser una nueva URL que incluya '/Home/Privacy'
+
+        cy.url().should('include', '/Home/Privacy')
+    })
+})
---
+++ end2end/end2end/cypress/integration/prueba_spec.js
@@ -0,0 +1,55 @@
+﻿describe('Mi primer test', function () {
+    it('No hace demasiado', function () {
+        expect(true).to.equal(true)
+    })
+
+    it('Visita una pagina', function () {
+        cy.visit('https://example.cypress.io')
+    })
+
+    it('Buscar el texto "type"', function () {
+        cy.visit('https://example.cypress.io')
+
+        cy.contains('type')
+    })
+
+    it('click en el link "type"', function () {
+        cy.visit('https://example.cypress.io')
+
+        cy.contains('type').click()
+    })
+
+    it('clickear "type" navega a una nueva url', () => {
+        cy.visit('https://example.cypress.io')
+
+        cy.contains('type').click()
+
+        cy.url().should('include', '/commands/actions')
+    })
+
+    it('Escribir en un input', () => {
+        cy.visit('https://example.cypress.io')
+
+        cy.contains('type').click()
+
+        cy.url().should('include', '/commands/actions')
+
+        cy.get('.action-email')
+        .type('fake@email.com')
+        .should('have.value', 'fake@email.com')
+    })
+
+    it('clicking "type" shows the right headings', function () {
+        cy.visit('https://example.cypress.io')
+
+        cy.pause()
+
+        cy.contains('type').click()
+
+        cy.url().should('include', '/commands/actions')
+
+        cy.get('.action-email')
+            .type('fake@email.com')
+            .should('have.value', 'fake@email.com')
+    })
+})
